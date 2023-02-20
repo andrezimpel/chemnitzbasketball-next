56bb9608c0ce8b89ca4ddf20126aaa30563616ea
Andre Zimpel <andrezimpel@users.noreply.github.com>
2023-02-20 18:29:06 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-import Event from './event'
+// import Event from './event'
 
-export default function IndexPage(): JSX.Element {
-  return (
-    <div>
-      <Event />
-      {/* <Video /> */}
-      {/* <div>form</div>
-      <div>messages</div>
-      <div>FAQ</div> */}
-    </div>
-  )
+// export default function IndexPage(): JSX.Element {
+//   return (
+//     <div>
+//       <Event />
+//       {/* <Video /> */}
+//       {/* <div>form</div>
+//       <div>messages</div>
+//       <div>FAQ</div> */}
+//     </div>
+//   )
+// }
+
+export default function Page() {
+  return <h1>Hello, Next.js!</h1>
 }
