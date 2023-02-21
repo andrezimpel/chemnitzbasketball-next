a834c9ebbb69b41838f6c1e9fbcfdbf0eff2d75f
Andre Zimpel <andrezimpel@users.noreply.github.com>
2023-02-21 18:27:50 +0100

    added heads

---
+++ src/app/newsletter/thank-you/head.tsx
@@ -0,0 +1,25 @@
+export default function Head() {
+  return (
+    <>
+      <title>Newsletter - CHEMNITZ BASKETBALL</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <meta name="theme-color" content="#a7a4e0" />
+      <meta data-react-helmet="true" name="twitter:description" content="Das Projekt CHEMNITZ BASKETBALL bringt die Liebe der Stadt für Kunst, Kultur und Sport zum Ausdruck. Helft uns, es umzusetzen!" />
+      <meta data-react-helmet="true" name="twitter:title" content="Startseite" />
+      <meta data-react-helmet="true" name="twitter:creator" content="@andrezimpel" />
+      <meta data-react-helmet="true" name="twitter:card" content="summary" />
+      <meta data-react-helmet="true" property="og:image" content="https://www.chemnitzbasketball.com/static/sharing-image-a86550113d05af929110169db1039096.jpg" />
+      <meta data-react-helmet="true" property="og:type" content="website" />
+      <meta data-react-helmet="true" property="og:description" content="Das Projekt CHEMNITZ BASKETBALL bringt die Liebe der Stadt für Kunst, Kultur und Sport zum Ausdruck. Helft uns, es umzusetzen!" />
+      <meta data-react-helmet="true" property="og:title" content="Startseite" />
+      <meta data-react-helmet="true" name="description" content="Das Projekt CHEMNITZ BASKETBALL bringt die Liebe der Stadt für Kunst, Kultur und Sport zum Ausdruck. Helft uns, es umzusetzen!" />
+    </>
+  )
+}
