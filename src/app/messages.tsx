eb8c037c1443d17c010c77eacc75315a12c0c16c
Andre Zimpel <andrezimpel@users.noreply.github.com>
2023-02-21 07:01:34 +0100

    added marquee

---
+++ src/app/messages.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import Marquee from "react-fast-marquee"
+
+export interface Message {
+  age: number
+  message: string
+  name: string
+}
+
+export default function Messages({ chunks }: { chunks: Message[][] }) {
+  const speeds = [40, 50, 55, 60, 65]
+
+  return (
+    <div className="space-y-4">
+      {chunks.map((chunk, i) => (
+        <Marquee
+          key={i}
+          speed={speeds[i]}
+          gradient={false}
+          pauseOnHover={true}
+        >
+          <div className="flex space-x-4">
+            {chunk.map((message, j) => (
+              <div key={j} className="inline-block rounded-md bg-purple-800 text-purple-50 p-4 font-overpass flex flex-col justify-center">
+                <div className="max-w-xl">{message.message}</div>
+                <div className="text-sm text-purple-300">- {message.name}{message.age && `(${message.age})`}</div>
+              </div>
+            ))}
+          </div>
+        </Marquee>
+      ))}
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,48 @@ import About from './about'
 import Architecture from './architecture'
 import Event from './event'
 import Faq from './faq'
+import Messages, { Message } from './messages'
 
-export default function IndexPage(): JSX.Element {
+function splitToChunks(array: Message[], parts: number) {
+  let result = []
+  for (let i = parts; i > 0; i--) {
+    result.push(array.splice(0, Math.ceil(array.length / i)))
+  }
+  return result
+}
+
+async function getMessages() {
+  // fetch from graphql endpoint
+  const res = await fetch('https://api.chemnitzbasketball.com/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: `
+        {
+          messages {
+            age
+            message
+            name
+          }
+        }
+      `
+    })
+  })
+  const { data } = await res.json()
+  return splitToChunks(data?.messages.filter((m: Message) => m.message.length < 140).sort((a: Message, b: Message) => (a.message.length > b.message.length) ? 1 : -1), 5)
+}
+
+export default async function IndexPage() {
+  const data = await getMessages()
   return (
     <div className="space-y-8 md:space-y-14">
       <Event />
+      <Messages chunks={data} />
       <Architecture />
       <About />
       <Faq />
-      {/* <Video /> */}
-      {/* <div>form</div>
-      <div>messages</div>
-      <div>FAQ</div> */}
     </div>
   )
 }
