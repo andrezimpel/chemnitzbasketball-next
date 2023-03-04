95faa38d0b018dabab15c91cce85e13f9ea28a4a
Andre Zimpel <andrezimpel@users.noreply.github.com>
2023-03-04 13:51:25 +0100

    thank you page

--- src/app/newsletter/thank-you/page.tsx
+++ src/app/newsletter/thank-you/page.tsx
@@ -7,7 +7,7 @@ export default function NewsletterThankYouPage(): JSX.Element {
         </div>
         <div className="space-y-4">
           <p className='md:text-lg text-justify'><span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-100 to-purple-300 animate-pulse duration-100 delay-100">Ohne dich könnten wir das Projekt nicht umsetzen. 💜</span></p>
-          <p className='md:text-lg text-justify'>Du kannst uns gern noch unterstützen, indem du die Präsentation am 4. März um 16.00 Uhr in der Messe mit deinen Freund:innen teilst. Je mehr Teilnehmer:innen wir sind, desto größer ist die Chance, dass die Stadt ein neues Basketballfeld baut!</p>
+          <p className='md:text-lg text-justify'>Wir halten dich in unserem Newsletter auf dem Laufenden.</p>
         </div>
       </div>
     </div>
